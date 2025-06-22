7a3685c32c9bfcf2ea8649c42fb605e39ce3ae3d
Dirk van der Laarse <dirk@laarse.co.za>
2025-06-22 15:52:33 +0000

    fix: handle guset permission for hook method

--- helpdesk_whatsapp/overrides/helpdesk/hd_ticket.py
+++ helpdesk_whatsapp/overrides/helpdesk/hd_ticket.py
@@ -1,3 +1,4 @@
+import frappe
 from helpdesk.helpdesk.doctype.hd_ticket.hd_ticket import HDTicket
 
 
@@ -35,3 +36,33 @@ class CustomHDTicket(HDTicket):
 
 		# Call the original method for other cases
 		return super().skip_email_workflow()
+
+	def on_communication_update(self, c):
+		"""
+		Override the on_communication_update method to handle guest users
+		"""
+		# =====================================================================================
+		# Original code from hd_ticket.py
+		# =====================================================================================
+		# If communication is incoming, then it is a reply from customer, and ticket must
+		# be reopened.
+		if c.sent_or_received == "Received":
+			self.status = "Open"
+		# If communication is outgoing, it must be a reply from agent
+		if c.sent_or_received == "Sent":
+			# Set first response date if not set already
+			self.first_responded_on = self.first_responded_on or frappe.utils.now_datetime()
+
+			if frappe.db.get_single_value("HD Settings", "auto_update_status"):
+				self.status = "Replied"
+
+		# Fetch description from communication if not set already. This might not be needed
+		# anymore as a communication is created when a ticket is created.
+		self.description = self.description or c.content
+		# Save the ticket, allowing for hooks to run.
+		# self.save()
+		# =====================================================================================
+		# Custom code
+		# =====================================================================================
+		self.save(ignore_permissions=True)
+		# =====================================================================================
